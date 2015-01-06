35af8042e08427131a92bbc087cf2c1fe083922c
Jens Bissinger <mail@jens-bissinger.de>
2015-01-06 21:17:00 +0100

    Improve error handling.

--- lib/hbci4jruby/web/turnover.rb
+++ lib/hbci4jruby/web/turnover.rb
@@ -2,14 +2,14 @@ module Hbci4jruby
   module Web
     class Turnover < ::Sinatra::Base
       get('/turnover') do
-        data = ::JSON.parse request.body.read
-        halt 'Please provide banking_passport' unless data["banking_passport"]
-        halt 'Please provide start_date' unless data["start_date"]
-        halt 'Please provide end_date' unless data["end_date"]
-        banking_passport = Hbci4jruby::BankingPassport.new
-        banking_passport.attributes = data["banking_passport"]
-        turnover = Hbci4jruby::Turnover.new
         begin
+          data = ::JSON.parse request.body.read
+          halt 'Please provide banking_passport' unless data["banking_passport"]
+          halt 'Please provide start_date' unless data["start_date"]
+          halt 'Please provide end_date' unless data["end_date"]
+          banking_passport = Hbci4jruby::BankingPassport.new
+          banking_passport.attributes = data["banking_passport"]
+          turnover = Hbci4jruby::Turnover.new
           result = turnover.list(banking_passport, {
             'start_date' => Time.new(data["start_date"]),
             'end_date'   => Time.new(data["end_date"])
